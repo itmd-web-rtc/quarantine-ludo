f7afd9e89d6cb3e97541f69f28ec4cfd54d47d78
dhirajj14 <djain14@hawk.iit.edu>
2020-11-03 16:02:15 -0600

    add singnaling channel

--- quarantine-ludo/routes/index.js
+++ quarantine-ludo/routes/index.js
@@ -1,14 +1,14 @@
-'use strict';
+"use strict";
 
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 var generateRoomURL = require("../lib/generate-random-url");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Quarantine Ludo' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Quarantine Ludo" });
 });
-  
+
 /* GET create room. */
 router.get("/create-room", function (req, res, next) {
   res.redirect(`/${generateRoomURL.randomRoom(3, 4, 3)}`);
@@ -17,8 +17,7 @@ router.get("/create-room", function (req, res, next) {
 /* GET Endpoint to accept the correct URL */
 router.get("/:room([a-z]{3}-[a-z]{4}-[a-z]{3}$)", function (req, res, next) {
   const namespace = req.params["room"];
-  res.render("index", { title: `Room ${namespace}`, namespace: namespace });
+  res.render("room", { title: `Room ${namespace}`, namespace: namespace });
 });
-
 
 module.exports = router;
