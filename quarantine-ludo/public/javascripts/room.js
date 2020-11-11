b7a2565d319871054e2d9c3b614e3ed383040c0b
Dhiraj Jain <54300222+dhirajj14@users.noreply.github.com>
2020-11-11 10:21:24 -0600

    Rtc data channel feature - Add text-chat data channel (#22)
    * chat chat log form with css update
    
    * add https server for app
    
    * remove unwanted media stream code
    
    * add stream and tracks and create pc
    
    * add negotiation and video call between slef & peer
    
    * add safari candidate fix
    
    * add perfect negotiation for safari
    
    * declare DOM chat elements
    
    * change css peer style name
    
    * add text-chat data channel
    
    * remove static message
    
    Co-authored-by: Dhiraj Jain <dhirajjain@Dhirajs-MacBook-Pro.local>

--- quarantine-ludo/public/javascripts/room.js
+++ quarantine-ludo/public/javascripts/room.js
@@ -16,6 +16,79 @@ var rtc_config = null;
 //Setting basic to get peer connection
 var pc = new RTCPeerConnection(rtc_config);
 
+//set data channel
+var dc = null;
+
+//declare DOM elements for chat
+var chatLog = document.querySelector('#chat-log');
+var chatForm = document.querySelector('#chat-form');
+var chatInput = document.querySelector('#message');
+var chatButton = document.querySelector('#send-button');
+
+
+function appendMsgToChatLog(log, msg, who){
+
+  var li = document.createElement('li');
+  var msg = document.createTextNode(msg);
+  li.appendChild(msg);
+  li.className = who;
+  log.appendChild(li);
+  if(chatLog.scrollTo){
+    chatLog.scrollTo({
+      top: chatLog.scrollHeight,
+      behavior: 'smooth'
+    });
+  }else{
+    chatLog.scrollTop = chatLog.scrollHeight;
+  }
+}
+
+
+function addDataChannelEventListner(datachannel){
+
+  datachannel.onmessage = function(e){
+    appendMsgToChatLog(chatLog, e.data, 'peer');
+  }
+
+  datachannel.onopen = function(){
+    chatButton.disabled = false;
+    chatInput.disabled = false;
+  }
+
+  datachannel.onclose = function(){
+    chatButton.disabled = true;
+    chatInput.disabled = true;
+  }
+
+  chatForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    var msg = chatInput.value;
+    appendMsgToChatLog(chatLog, msg, 'self');
+    datachannel.send(msg);
+    chatInput.value = '';
+  });
+
+}
+
+
+//Once the RTC connection is steup and connected the peer will open data channel
+pc.onconnectionstatechange = function(e){
+  if(pc.connectionState == 'connected'){
+    if(clientIs.polite) {
+      console.log("Creating a data channel on the initiating side");
+      dc = pc.createDataChannel('text chat');
+      addDataChannelEventListner(dc);
+    }
+  }
+}
+
+//listen for datachannel
+// This will on fire on receiving end of the connection
+pc.ondatachannel = function(e){
+  console.log("Data Channel is open");
+  dc = e.channel;
+  addDataChannelEventListner(dc);
+}
 
 //video Streams
 var media_constraints = {video: true, audio: false};
