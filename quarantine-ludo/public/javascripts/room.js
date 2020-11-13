1ad1a5e1b0933b68906704a2804067ec8febb684
Dhiraj Jain <dhirajjain@Dhirajs-MacBook-Pro.local>
2020-11-12 20:59:12 -0600

    refactor signaling state refer to w3c

--- quarantine-ludo/public/javascripts/room.js
+++ quarantine-ludo/public/javascripts/room.js
@@ -8,7 +8,8 @@ sc.on("message", function (data) {
 var clientIs = {
   makingOffer: false,
   ignoringOffer: false,
-  polite:false
+  polite:false,
+  settingRemoteAnswerPending: false
 }
 
 var rtc_config = null;
@@ -165,14 +166,27 @@ sc.on('signal', async function({candidate, description}){
   try {
     if(description){
       console.log("Received a description!!!");
-      var OfferCollision = (description.type == 'offer')  && (clientIs.makingOffer  || pc.signalingState != 'stable');
-      clientIs.ignoringOffer = !clientIs.polite && OfferCollision;
+      var readyForOffer = !clientIs.makingOffer && (pc.signalingState == "stable" || clientIs.settingRemoteAnswerPending);
+
+      var offerCollision = description.type == "answer" && !readyForOffer;
+
+      clientIs.ignoringOffer = !clientIs.polite && offerCollision;
+
       if(clientIs.ignoringOffer){
         return;
       }
 
       // Set the remote decription
+     // Set the remote description...
+     try {
+      console.log('Trying to set a remote description:\n', description);
+      clientIs.settingRemoteAnswerPending = description.type == "answer";
       await pc.setRemoteDescription(description);
+      clientIs.settingRemoteAnswerPending = false;
+      } catch(error) {
+      console.error('Error from setting local description', error);
+      }
+
 
       //if it's offer you need to answer
       if(description.type == 'offer'){
@@ -182,8 +196,17 @@ sc.on('signal', async function({candidate, description}){
           await pc.setLocalDescription();
         } catch (error) {
           //works for older browsers we pass the answer we created using RTCSession
-          var answer = await pc.createAnswer();
-          await pc.setLocalDescription(new RTCSessionDescription(answer));
+          if (pc.signalingState == 'have-remote-offer') {
+            // create a answer, if that's what's needed...
+            console.log('Trying to prepare an answer:');
+            var offer = await pc.createAnswer();
+          } else {
+            // otherwise, create an offer
+            console.log('Trying to prepare an offer:');
+            var offer = await pc.createOffer();
+          }
+         
+          await pc.setLocalDescription(new RTCSessionDescription(offer));
         } finally{
           sc.emit('signal', {description: pc.localDescription});
         }    
@@ -193,8 +216,14 @@ sc.on('signal', async function({candidate, description}){
       console.log('Received a candidate:');
       console.log(candidate);
       //safari fix for the blank candidate
-      if(candidate.candidate.length > 1){
-        await pc.addIceCandidate(candidate);
+      try {
+        if (candidate.candidate.length > 1) {
+          await pc.addIceCandidate(candidate);
+        }
+      } catch(error) {
+        if (!clientIs.ignoringOffer) {
+          throw error;
+        }
       }
       
     }
