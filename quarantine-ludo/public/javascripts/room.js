2fd15cdfd1b4efb06053c246f914977edbe0fde3
Noor Fathima <noorfathima@NoorFathima-MacBook-Pro.local>
2020-11-15 16:07:23 +0530

    WIP

--- quarantine-ludo/public/javascripts/room.js
+++ quarantine-ludo/public/javascripts/room.js
@@ -8,9 +8,9 @@ sc.on("message", function (data) {
 var clientIs = {
   makingOffer: false,
   ignoringOffer: false,
-  polite:false,
-  settingRemoteAnswerPending: false
-}
+  polite: false,
+  settingRemoteAnswerPending: false,
+};
 
 var rtc_config = null;
 
@@ -21,144 +21,144 @@ var pc = new RTCPeerConnection(rtc_config);
 var dc = null;
 
 //declare DOM elements for chat
-var chatLog = document.querySelector('#chat-log');
-var chatForm = document.querySelector('#chat-form');
-var chatInput = document.querySelector('#message');
-var chatButton = document.querySelector('#send-button');
-var joinForm = document.querySelector('#join-form');
-var joinName = document.querySelector('#join-name');
+var chatLog = document.querySelector("#chat-log");
+var chatForm = document.querySelector("#chat-form");
+var chatInput = document.querySelector("#message");
+var chatButton = document.querySelector("#send-button");
+var joinForm = document.querySelector("#join-form");
+var joinName = document.querySelector("#join-name");
 
-function appendMsgToChatLog(log, msg, who){
-
-  var li = document.createElement('li');
+function appendMsgToChatLog(log, msg, who) {
+  var li = document.createElement("li");
 
   //Add timestampn to chat messages
-  var br = document.createElement('br');
-  var span = document.createElement('span');
+  var br = document.createElement("br");
+  var span = document.createElement("span");
   span.className = "chat-time";
-
 
   var msg = document.createTextNode(msg);
   li.appendChild(msg);
   li.className = who;
-  if(who !== "join"){
+  if (who !== "join") {
     li.appendChild(br);
     li.appendChild(span);
   }
 
   //add current timestamp
-  span.innerText = new Date().toLocaleTimeString('en-US', { hour12: true, hour: "numeric", minute: "numeric"});
+  span.innerText = new Date().toLocaleTimeString("en-US", {
+    hour12: true,
+    hour: "numeric",
+    minute: "numeric",
+  });
   log.appendChild(li);
-  if(chatLog.scrollTo){
+  if (chatLog.scrollTo) {
     chatLog.scrollTo({
       top: chatLog.scrollHeight,
-      behavior: 'smooth'
+      behavior: "smooth",
     });
-  }else{
+  } else {
     chatLog.scrollTop = chatLog.scrollHeight;
   }
 }
 
+function addDataChannelEventListner(datachannel) {
+  datachannel.onmessage = function (e) {
+    appendMsgToChatLog(chatLog, e.data, "peer");
+  };
 
-function addDataChannelEventListner(datachannel){
-
-  datachannel.onmessage = function(e){
-    appendMsgToChatLog(chatLog, e.data, 'peer');
-  }
-
-  datachannel.onopen = function(){
+  datachannel.onopen = function () {
     chatButton.disabled = false;
     chatInput.disabled = false;
-  }
+  };
 
-  datachannel.onclose = function(){
+  datachannel.onclose = function () {
     chatButton.disabled = true;
     chatInput.disabled = true;
-  }
+  };
 
-  chatForm.addEventListener('submit', function(e){
+  chatForm.addEventListener("submit", function (e) {
     e.preventDefault();
     var msg = chatInput.value;
     msg = msg.trim();
-    if(msg !== ""){
-      appendMsgToChatLog(chatLog, msg, 'self');
+    if (msg !== "") {
+      appendMsgToChatLog(chatLog, msg, "self");
       datachannel.send(msg);
-      chatInput.value = '';
+      chatInput.value = "";
     }
   });
-
 }
 
-
 //Once the RTC connection is steup and connected the peer will open data channel
-pc.onconnectionstatechange = function(e){
-  if(pc.connectionState == 'connected'){
-    if(clientIs.polite) {
+pc.onconnectionstatechange = function (e) {
+  if (pc.connectionState == "connected") {
+    if (clientIs.polite) {
       console.log("Creating a data channel on the initiating side");
-      dc = pc.createDataChannel('text chat');
+      dc = pc.createDataChannel("text chat");
       addDataChannelEventListner(dc);
     }
   }
-}
+};
 
 //listen for datachannel
 // This will on fire on receiving end of the connection
-pc.ondatachannel = function(e){
+pc.ondatachannel = function (e) {
   console.log("Data Channel is open");
   dc = e.channel;
   addDataChannelEventListner(dc);
-}
+};
 
 //video Streams
-var media_constraints = {video: true, audio: false};
+var media_constraints = { video: true, audio: false };
 
-var selfVideo = document.querySelector('#self-video');
+var selfVideo = document.querySelector("#self-video");
 var selfStream = new MediaStream();
 selfVideo.srcObject = selfStream;
 
-var peerVideo = document.querySelector('#peer-video');
+var peerVideo = document.querySelector("#peer-video");
 var peerStream = new MediaStream();
 console.log(peerStream);
 peerVideo.srcObject = peerStream;
 
-
 async function startStream(name) {
-  try{
+  try {
     var stream = await navigator.mediaDevices.getUserMedia(media_constraints);
-    for( var track of stream.getTracks()){
+    for (var track of stream.getTracks()) {
       pc.addTrack(track);
     }
 
     selfVideo.srcObject = stream;
     //send joined message with current timestamp
-    sc.emit('joined', `${name} joined the chat! at ${new Date().toLocaleTimeString('en-US', 
-                                                    { hour12: true, hour: "numeric", minute: "numeric"})}`);
-  } catch(error){
-
-  }
+    sc.emit(
+      "joined",
+      `${name} joined the chat! at ${new Date().toLocaleTimeString("en-US", {
+        hour12: true,
+        hour: "numeric",
+        minute: "numeric",
+      })}`
+    );
+  } catch (error) {}
 }
 
-sc.on('joined', function(e){
-  appendMsgToChatLog(chatLog,e,"join");
-})
+sc.on("joined", function (e) {
+  appendMsgToChatLog(chatLog, e, "join");
+});
 
 pc.ontrack = (track) => {
   peerStream.addTrack(track.track);
-}
+};
 
-var callButton = document.querySelector('#join-button');
+var callButton = document.querySelector("#join-button");
 
-callButton.addEventListener('click', function(e){
+callButton.addEventListener("click", function (e) {
   e.preventDefault();
-  if(joinName.value !== ""){
+  if (joinName.value !== "") {
     joinCall(joinName.value);
-  }else{
-    alert('Enter your Name!');
+  } else {
+    alert("Enter your Name!");
   }
 });
 
-
-function joinCall(name){
+function joinCall(name) {
   clientIs.polite = true;
   negotiateConnection();
   startStream(name);
@@ -166,7 +166,7 @@ function joinCall(name){
 }
 
 async function negotiateConnection() {
-  pc.onnegotiationneeded = async function() {
+  pc.onnegotiationneeded = async function () {
     try {
       console.log("Making Offer");
       clientIs.makingOffer = true;
@@ -174,82 +174,81 @@ async function negotiateConnection() {
         await pc.setLocalDescription();
       } catch (error) {
         var offer = await pc.createOffer();
-          await pc.setLocalDescription(new RTCSessionDescription(offer));
-      }finally{
-        sc.emit('signal', { description: pc.localDescription});
+        await pc.setLocalDescription(new RTCSessionDescription(offer));
+      } finally {
+        sc.emit("signal", { description: pc.localDescription });
       }
     } catch (error) {
       console.log(error);
-    }finally{
+    } finally {
       clientIs.makingOffer = false;
     }
-  }
+  };
 }
 
-sc.on('signal', async function({candidate, description}){
+sc.on("signal", async function ({ candidate, description }) {
   try {
-    if(description){
+    if (description) {
       console.log("Received a description!!!");
-      var readyForOffer = !clientIs.makingOffer && (pc.signalingState == "stable" || clientIs.settingRemoteAnswerPending);
+      var readyForOffer =
+        !clientIs.makingOffer &&
+        (pc.signalingState == "stable" || clientIs.settingRemoteAnswerPending);
 
       var offerCollision = description.type == "answer" && !readyForOffer;
 
       clientIs.ignoringOffer = !clientIs.polite && offerCollision;
 
-      if(clientIs.ignoringOffer){
+      if (clientIs.ignoringOffer) {
         return;
       }
 
       // Set the remote decription
-     // Set the remote description...
-     try {
-      console.log('Trying to set a remote description:\n', description);
-      clientIs.settingRemoteAnswerPending = description.type == "answer";
-      await pc.setRemoteDescription(description);
-      clientIs.settingRemoteAnswerPending = false;
-      } catch(error) {
-      console.error('Error from setting local description', error);
+      // Set the remote description...
+      try {
+        console.log("Trying to set a remote description:\n", description);
+        clientIs.settingRemoteAnswerPending = description.type == "answer";
+        await pc.setRemoteDescription(description);
+        clientIs.settingRemoteAnswerPending = false;
+      } catch (error) {
+        console.error("Error from setting local description", error);
       }
 
-
       //if it's offer you need to answer
-      if(description.type == 'offer'){
+      if (description.type == "offer") {
         console.log("Offer description");
         try {
           //works for latest browsers
           await pc.setLocalDescription();
         } catch (error) {
           //works for older browsers we pass the answer we created using RTCSession
-          if (pc.signalingState == 'have-remote-offer') {
+          if (pc.signalingState == "have-remote-offer") {
             // create a answer, if that's what's needed...
-            console.log('Trying to prepare an answer:');
+            console.log("Trying to prepare an answer:");
             var offer = await pc.createAnswer();
           } else {
             // otherwise, create an offer
-            console.log('Trying to prepare an offer:');
+            console.log("Trying to prepare an offer:");
             var offer = await pc.createOffer();
           }
-         
-          await pc.setLocalDescription(new RTCSessionDescription(offer));
-        } finally{
-          sc.emit('signal', {description: pc.localDescription});
-        }    
-      }
 
-    }else if(candidate){
-      console.log('Received a candidate:');
+          await pc.setLocalDescription(new RTCSessionDescription(offer));
+        } finally {
+          sc.emit("signal", { description: pc.localDescription });
+        }
+      }
+    } else if (candidate) {
+      console.log("Received a candidate:");
       console.log(candidate);
       //safari fix for the blank candidate
       try {
         if (candidate.candidate.length > 1) {
           await pc.addIceCandidate(candidate);
         }
-      } catch(error) {
+      } catch (error) {
         if (!clientIs.ignoringOffer) {
           throw error;
         }
       }
-      
     }
   } catch (error) {
     console.log(error);
@@ -257,6 +256,16 @@ sc.on('signal', async function({candidate, description}){
 });
 
 //logic to send candidate
-pc.onicecandidate = function({candidate}){
-  sc.emit('signal', {candidate: candidate});
+pc.onicecandidate = function ({ candidate }) {
+  sc.emit("signal", { candidate: candidate });
+};
+
+function setWidthAndHeight() {
+  console.log("here")
+  var homeBlockGreen = document.querySelector("#home-block-green");
+  var leftLayer = document.querySelector("#left-layer");
+  homeBlockGreen.style.width ='100%';
+  homeBlockGreen.width = homeBlockGreen.offsetWidth;
 }
+
+setWidthAndHeight()
